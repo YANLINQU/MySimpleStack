432197271e03dad8cc4f447124d225c5a83202ff
YANLINQU <quyanlingg@hotmail.com>
2016-02-19 15:06:30 +0100

    first commit

---
+++ src/main/java/dcll/DCLL_TP2/App.java
@@ -0,0 +1,13 @@
+package dcll.DCLL_TP2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
